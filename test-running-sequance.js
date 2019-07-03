db3ddc531d6e7eba4a4b657e59590ea615e320f1
Krishank Dwivedi <krishank.dwivedi@akqa.com>
2019-07-03 15:11:53 +1000

    First working Draft

---
+++ test-running-sequance.js
@@ -0,0 +1,2 @@
+// this need some implementation but we can also allow files to be run im some sequance
+// whihc we can define in JSON or else it will read it from the test folder this funcationality still need to be implemented
